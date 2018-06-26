902a7325c5a3bc193ed85aca568d1c04c19db5a5
Fred Feng Coding Center <fred.feng0326@gmail.com>
2018-06-26 23:50:51 +0800

    update 2018.06.26

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-    推荐页面
+  <div class="recommend">
+    <div class="recommend-content">
+      <div class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
+      </div>
+      <div class="recommend-lsit">
+        <h1 class="list-title">热门歌曲推荐</h1>
+        <ul></ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import {getRecommend} from 'api/recommend'
   import {ERR_OK} from 'api/config'
+  import Slider from 'base/slider/slider'
   export default {
+    data() {
+      return {
+        recommends: []
+      }
+    },
     created() {
       this._getRecommed()
     },
@@ -15,10 +35,13 @@
       _getRecommed() {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.slider)
+            this.recommends = res.data.slider
           }
         })
       }
+    },
+    comments: {
+      Slider
     }
   }
 

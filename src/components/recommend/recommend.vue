19718f48109b3d1531cf2d198f7562cda7d40104
fred <fred@9an.com>
2018-06-26 18:41:56 +0800

    Initial commit

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    推荐页面
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import {getRecommend} from 'api/recommend'
+  import {ERR_OK} from 'api/config'
+  export default {
+    created() {
+      this._getRecommed()
+    },
+    methods: {
+      _getRecommed() {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.slider)
+          }
+        })
+      }
+    }
+  }
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
